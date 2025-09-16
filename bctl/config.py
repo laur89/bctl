f238e090d6ba9274b9c5fcaf278718d34d0beede
laur <laur.aliste@gmail.com>
2025-09-16 15:41:34 +0200

    config: improve docs

--- bctl/config.py
+++ bctl/config.py
@@ -60,14 +60,14 @@ class Conf(TypedDict):
 
 
 default_conf: Conf = {
-    'log_lvl': 'INFO',
+    'log_lvl': 'INFO',  # daemon log level, doesn't apply to the client
     'ddcutil_bus_path_prefix': '/dev/i2c-',  # prefix to the bus number
-    'ddcutil_brightness_feature': '10',  # str!
+    'ddcutil_brightness_feature': '10',
     'ddcutil_svcp_flags': ['--skip-ddc-checks'],  # flags passed to [ddcutil setvcp] commands
     'ddcutil_gvcp_flags': [],  # flags passed to [ddcutil getvcp] commands
     'monitor_udev': True,  # monitor udev events for drm subsystem to detect ext. display (dis)connections
     'periodic_init_sec': 0,  # periodically re-init/re-detect monitors; 0 to disable
-    'sync_brightness': False,  # try to keep all displays' brightnesses at same value/synchronized
+    'sync_brightness': False,  # keep all displays' brightnesses at same value/synchronized
     'sync_strategy': 'MEAN',  # if displays' brightnesses differ and are synced, what value to sync them to; only active if sync_brightness=True;
                               # 'MEAN' = set to arithmetic mean, 'LOW' = set to lowest, 'HIGH' = set to highest
     'notify': {
@@ -87,18 +87,18 @@ default_conf: Conf = {
     'udev_event_debounce_sec': 3.0,  # both for debouncing & delay; have experienced missed ext. display detection w/ 1.0
     'msg_consumption_window_sec': 0.1,  # can be set to 0 if no delay/window is required
     'brightness_step': 5,  # %
-    'ignored_displays': [],  # either ddcutil's "Monitor:" value, or <device> in /sys/class/backlight/<device>
-    'ignore_internal_display': False,  # do not control internal display if available
+    'ignored_displays': [],  # either [ddcutil --brief detect] cmd "Monitor:" value, or <device> in /sys/class/backlight/<device>
+    'ignore_internal_display': False,  # do not control internal (i.e. laptop) display if available
     'ignore_external_display': False,  # do not control external display(s) if available
     'main_display_ctl': 'DDCUTIL',  # RAW | DDCUTIL | BRIGHTNESSCTL | BRILLO
     'internal_display_ctl': 'RAW',  # RAW | BRIGHTNESSCTL | BRILLO;  only used if main_display_ctl=DDCUTIL and we're a laptop
     'raw_device_dir': '/sys/class/backlight',  # used if main_display_ctl=RAW OR
-                                               # (main_display_ctl=DDCUTIL and internal_display_ctl=RAW AND we're a laptop)
-    'fatal_exit_code': 100,  # exit code signifying fatal exit code, should not be retried;
+                                               # (main_display_ctl=DDCUTIL AND internal_display_ctl=RAW AND we're a laptop)
+    'fatal_exit_code': 100,  # exit code signifying fatal exit that should not be retried;
                              # you might want to use this value in systemd unit file w/ RestartPreventExitStatus config
     'socket_path': '/tmp/.bctld-ipc.sock',
     'sim': None,  # simulation config, will be set by sim client
-    'state_f_path': '/tmp/.bctld.state',
+    'state_f_path': '/tmp/.bctld.state',  # state that should survive restarts are stored here
     'state': None  # do not set, will be read in from state_f_path
 }
 
